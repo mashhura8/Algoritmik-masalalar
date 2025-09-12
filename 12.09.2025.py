5353f3f06df5d68428c32a033e64c6470c7cc164
mashhura8 <mashhurakarimboyeva8@gmail.com>
2025-09-12 16:55:47 +0500

    masalalar yechimi

---
+++ 12.09.2025.py
@@ -0,0 +1,61 @@
+# 1-masala
+ism = "Mashhura"
+print("Salom", ism )
+
+
+# 2-masala
+yosh = "17"
+print("Mening yoshim", yosh)
+
+
+# 3-masala
+yosh = 17
+hozirgi_yil = 2025
+print("Tug'ilgan yil" , hozirgi_yil - yosh )
+
+
+# 4-masala
+a = 3
+b = 5
+print("Ikki son yig'indisi" , a+b)
+
+
+# 5-masala
+uzunlik = 10
+eni = 5
+print("To'rtburchak yuzi", uzunlik*eni)
+
+
+# 6-masala
+
+
+
+# 7-masala
+ism = "Mashhura"
+yosh = "17"
+print("Mening ismim" , ism , "yoshim" , yosh )
+
+
+
+# 8-masala
+
+
+
+
+# 9-masala
+
+
+
+
+# 10-masala
+a = 1
+b = 2
+c = 4
+print( "Sonning o'rtachasi" , a+b+c / 3)
+
+
+
+
+
+
+
